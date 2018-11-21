14de328eac1d639e3901145a3cafa92fcaf00f2b
Marcos J. Alvarez <malvarez@belatrixsf.com>
2018-11-21 10:03:49 -0500

    Add schema for simulator

--- src/app/pages/admin/index.ts
+++ src/app/pages/admin/index.ts
@@ -1,6 +1,7 @@
 import {EnvironmentListInternalComponents} from './environment-list/environment-list.component';
 import {CreateEnvironmentInternalComponents} from './environment-create/environment-create.component';
 import {EditEnvironmentInternalComponents} from './environment-create/environment-edit.component';
+import {AdminSimulatorComponents, AdminSimulatorEntryComponents} from './simulator';
 import {CreateEnvironmentTypeComponent} from './environment-type-create/environment-type-create.component';
 
 /**
@@ -11,8 +12,10 @@ export const AdminPagesComponents = [
   ...EnvironmentListInternalComponents,
   ...CreateEnvironmentInternalComponents,
   ...EditEnvironmentInternalComponents,
+  ...AdminSimulatorComponents,
 ];
 
 export const AdminEntryComponents = [
   CreateEnvironmentTypeComponent,
+  ...AdminSimulatorEntryComponents,
 ];
